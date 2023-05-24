8422e55b3e104584b8b191b18fbee7700631cd9a
Rennysoares <117554741+Rennysoares@users.noreply.github.com>
2023-05-24 17:10:24 -0400

    teste

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect  } from 'react';
 import { StatusBar } from 'expo-status-bar';
-
 import { 
   StyleSheet, 
   Text, 
@@ -8,110 +7,86 @@ import {
   TextInput,
   Button,
   Alert,
-  Modal,
-  VirtualizedList
+  FlatList, TouchableOpacity
 } from 'react-native';
 
-export default function App() {
-  const[value1, setValue1] = useState("");
-  const[value2, setValue2] = useState("");
-  const[value3, setValue3] = useState("");
-  const[visibleModal, setVisibleModal] = useState(true);
+import * as SQLite from "expo-sqlite";
 
-  function CalculaMaiorValor(){
-    const maiorValor = Math.max(parseInt(value1), parseInt(value2), parseInt(value3))
-    Alert.alert('Maior Valor', 'O maior valor é: ' + maiorValor);
+export default function App() {
+  const db = SQLite.openDatabase('dbName');
+
+  db.transaction(tx => {
+    tx.executeSql(
+      'CREATE TABLE IF NOT EXISTS MyTable (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(50))'
+    );
+    tx.executeSql(
+      'INSERT INTO MyTable (name) VALUES (?)',
+      ['Name'],
+      () => {
+        console.log('Data inserted successfully.');
+      },
+      error => {
+        console.log('Error inserting data: ', error);
+      }
+    );
+  });
+  
+  function consultaDados(){
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM MyTable', [], (_, { rows }) => {
+        console.log(rows._array);
+      });
+    });
   }
-  function CalculaMenorValor(){
-    const menorValor = Math.min(parseInt(value1), parseInt(value2), parseInt(value3))
-    Alert.alert('Menor Valor', 'O menor valor é: ' + menorValor);
+
+  function inserirDados(){
+    db.transaction(tx => {
+      tx.executeSql(
+        'INSERT INTO MyTable (name) VALUES (?)',
+        ['John Doe'],
+        () => {
+          console.log('Data inserted successfully.');
+        },
+        error => {
+          console.log('Error inserting data: ', error);
+        }
+      );
+    });
+  }
+
+  function deletarDados(){
+    db.transaction(tx => {
+      tx.executeSql('DELETE FROM MyTable', () => {
+        console.log('Delete successfull');
+      });
+      error => {
+        console.log('Error inserting data: ', error);
+      }
+      console.log('Delete successfull');
+    });
   }
   return (
-    <View style={styles.container}>
-      <Text>Aplicativo para achar o maior valor</Text>
-      <TextInput
-        style={styles.textInput}
-        value={value1}
-        onChangeText={setValue1}
-        keyboardType='numeric'
-        placeholder='valor1'
+    <View>
+      <StatusBar
+        animated={true}
+        backgroundColor="rgb(100, 100, 100)"
       />
-      <TextInput
-        style={styles.textInput}
-        value={value2}
-        onChangeText={setValue2}
-        keyboardType='numeric'
-        placeholder='valor2'
-      />
-      <TextInput
-        style={styles.textInput}
-        value={value3}
-        onChangeText={setValue3}
-        keyboardType='numeric'
-        placeholder='valor3'
-      />
-      <StatusBar style="auto" />
-      <View style={styles.button}>
+      
+      <Text>TESTE</Text>
+      <Text>Teste</Text>
       <Button
-        title="Ache o maior valor"
-        onPress={CalculaMaiorValor}
+        title='consult'
+        onPress={consultaDados}
       />
-      </View>
-      <View>
       <Button
-        title="Ache o menor valor"
-        onPress={CalculaMenorValor}
+        title='insert data'
+        onPress={inserirDados}
       />
-      </View>
-      <Modal
-        transparent={visibleModal}
-        visible={visibleModal}
-      >
-        <View style={styles.modal}>
-        <View style={styles.box}>
-          <Text>Olá, bem vindo ao aplicativo</Text>
-          <Button
-            title="vamos começar"
-            onPress={()=>{setVisibleModal(false)}}
-          />
-        </View>
-        </View>
-      </Modal>
+      <Button
+        title='delete all data'
+        onPress={deletarDados}
+      />
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ddd',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textInput:{
-    borderWidth: 1,
-    width: 200,
-    borderRadius: 5,
-    padding: 3,
-    marginVertical: 5,
-    backgroundColor: '#fff'
-  },
-  modal:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)'
-  },
-  box:{
-    width: 300,
-    height: 150,
-    backgroundColor: 'rgb(200, 200, 0)',
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 20
-  },
-  button:{
-    marginVertical: 10
-  }
-});
